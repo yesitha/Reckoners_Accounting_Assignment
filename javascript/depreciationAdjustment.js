aacde590a27176b688fe8ed8b6b187159905c1c7
RashiniNethmini <s.a.r.nethmini@gmail.com>
2022-05-17 16:03:50 +0530

    depreciation JS file edited

--- javascript/depreciationAdjustment.js
+++ javascript/depreciationAdjustment.js
@@ -1,67 +1,37 @@
 
 function calculate(form) {
     var assetName=document.forms["form"]["A_name"].value;
-    var aAmount=document.forms["form"]["A_amount"].value;
-    // var pRate=document.forms["form"]["P_rate"].value;
-    // var pAmount=document.forms["form"]["P_amount"].value;
-    var type=document.forms["form"]["Radios1"].value;
-    // var NotOpenB=document.forms["form"]["DRadios2"].value;
-    var provision=(dAmount*pRate)/100;
-    var proB=pAmount-provision;
-    //var Iremaining=I_annualAmount-incomeAmount;
-  
+     var aValue=document.forms["form"]["A_value"].value;
+     var purchaseDate=document.forms["form"]["datepicker1"].value;
+     var evaluateDate=document.forms["form"]["datepicker2"].value;
+    // var ULife=document.forms["form"]["usefulLife"].value;
+    var drate=document.forms["form"]["dyear"].value;
+    var d1 = new Date(purchaseDate);  
+    var month=d.getMonth();
+    var remainMonth=12-month;
+    var forYear=(aValue*drate)/100;
+    var forMonth=forYear/12;
+    var aDepreciation=forMonth*remainMonth;
+    var netvalue=aValue-aDepreciation;
     
-        document.getElementById('Dout1').innerHTML = "Debtor Name :   "+assetName;
+        document.getElementById('Dout1').innerHTML = "Asset Name :   "+assetName;
+        // var month_diff =  - .getTime();
+        // document.getElementById('Dout2').innerHTML = "Asset Name :   "+year;
     
-    if (type=="line"){
+    if (drate!=" "){
         //No open balance
        
-        document.getElementById('dout2_1').innerHTML = " Provision for doubtful debt :  ";
-        document.getElementById('dout2_2').innerHTML ="Rs."+provision;
+        document.getElementById('Dout2_1').innerHTML = assetName+" depreciation :  ";
+        document.getElementById('Dout2_2').innerHTML ="Rs."+aDepreciation;
         
-        document.getElementById('dout3_1').innerHTML =" Debtor :  ";
-        document.getElementById('dout3_2').innerHTML =dAmount;
-        document.getElementById('dout3_3').innerHTML =" Provision for doubtful debt :  ";
-        document.getElementById('dout3_4').innerHTML ="("+provision+")";
-        document.getElementById('dout3_5').innerHTML =dAmount-provision;
-    
-    }else if (Open=="B"){
-       //with open balance
-        if(proB>0){
-            //when opening balance greater than
-        document.getElementById('dout2_1').innerHTML = " Over Provision :  ";
-        document.getElementById('dout2_2').innerHTML ="Rs. ("+proB+")";
-   
-        document.getElementById('dout3_1').innerHTML =" Debtor :  ";
-        document.getElementById('dout3_2').innerHTML =dAmount;
-        document.getElementById('dout3_3').innerHTML =" Provision for doubtful debt :  ";
-        document.getElementById('dout3_4').innerHTML ="("+provision+")";
-        document.getElementById('dout3_5').innerHTML =dAmount-provision;
-        }else if (proB<0){
-            //when opening balance less than
-            var PProB=-proB;
-        document.getElementById('dout2_1').innerHTML = " Under Provision  :  ";
-        document.getElementById('dout2_2').innerHTML ="Rs."+PProB;
-   
-        document.getElementById('dout3_1').innerHTML =" Debtor :  ";
-        document.getElementById('dout3_2').innerHTML =dAmount;
-        document.getElementById('dout3_3').innerHTML =" Provision for doubtful debt :  ";
-        document.getElementById('dout3_4').innerHTML ="("+provision+")";
-        document.getElementById('dout3_5').innerHTML =dAmount-provision;
-        }else{
-            document.getElementById('dout2_1').innerHTML = " Provision for doubtful debt :  ";
-            document.getElementById('dout2_2').innerHTML ="Rs."+proB;
-            
-            document.getElementById('dout3_1').innerHTML =" Debtor :  ";
-            document.getElementById('dout3_2').innerHTML =dAmount;
-            document.getElementById('dout3_3').innerHTML =" Provision for doubtful debt :  ";
-            document.getElementById('dout3_4').innerHTML ="("+provision+")";
-            document.getElementById('dout3_5').innerHTML =dAmount-provision;
-        }
+        document.getElementById('Dout3_1').innerHTML =assetName;
+        document.getElementById('Dout3_2').innerHTML =aValue;
+        document.getElementById('Dout3_3').innerHTML ="("+aDepreciation+")";
+        document.getElementById('Dout3_4').innerHTML =netvalue;
     }
     else{
         //no adjustments
-        document.getElementById('dout2').innerHTML = "         No adjustments          ";
+        document.getElementById('Dout2').innerHTML = "         No adjustments          ";
     }
    
 }
