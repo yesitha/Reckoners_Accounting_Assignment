6d01883c926c9d21b7df61659ce1775ee0b9b853
RashiniNethmini <s.a.r.nethmini@gmail.com>
2022-05-17 22:04:31 +0530

    Depreciation JS completed

--- javascript/depreciationAdjustment.js
+++ javascript/depreciationAdjustment.js
@@ -4,23 +4,41 @@ function calculate(form) {
      var aValue=document.forms["form"]["A_value"].value;
      var purchaseDate=document.forms["form"]["datepicker1"].value;
      var evaluateDate=document.forms["form"]["datepicker2"].value;
-    // var ULife=document.forms["form"]["usefulLife"].value;
+    var ULife=document.forms["form"]["usefulLife"].value;
+    var residual=document.forms["form"]["Rvv"].value;
     var drate=document.forms["form"]["dyear"].value;
+
     var d1 = new Date(purchaseDate);  
-    var month=d.getMonth();
-    var remainMonth=12-month;
-    var forYear=(aValue*drate)/100;
-    var forMonth=forYear/12;
-    var aDepreciation=forMonth*remainMonth;
+    // var day1=d1.getDate();
+    // var month1=d1.getMonth();
+    // var year1=d1.getFullYear();
+
+    var d2 = new Date(evaluateDate); 
+    // var day2=d2.getDay();
+    // var month2=d1.getMonth();
+    // var year2=d1.getFullYear();
+
+ 
+ 
+    var ptime = Math.abs(d2-d1);
+    var pdays = Math.ceil(ptime/(1000*60*60*24));
+
+    var foraYear=(aValue*drate)/100;
+    var foraMonth=foraYear/12;
+    var foraday=foraYear/365;
+  
+    var aDepreciation=pdays*foraday;
     var netvalue=aValue-aDepreciation;
+
+    var ulifeinDays=ULife*365;
     
-        document.getElementById('Dout1').innerHTML = "Asset Name :   "+assetName;
-        // var month_diff =  - .getTime();
-        // document.getElementById('Dout2').innerHTML = "Asset Name :   "+year;
+    document.getElementById('Dout1').innerHTML = "Asset Name :   "+assetName;
+      
     
-    if (drate!=" "){
-        //No open balance
-       
+   
+        if(ulifeinDays>=pdays){
+            document.getElementById('Dout2').innerHTML = "Depreciation for a year : Rs."+foraYear;
+            document.getElementById('Dout3').innerHTML = "Depreciation for a month :  Rs."+foraMonth;
         document.getElementById('Dout2_1').innerHTML = assetName+" depreciation :  ";
         document.getElementById('Dout2_2').innerHTML ="Rs."+aDepreciation;
         
@@ -28,11 +46,17 @@ function calculate(form) {
         document.getElementById('Dout3_2').innerHTML =aValue;
         document.getElementById('Dout3_3').innerHTML ="("+aDepreciation+")";
         document.getElementById('Dout3_4').innerHTML =netvalue;
-    }
-    else{
-        //no adjustments
-        document.getElementById('Dout2').innerHTML = "         No adjustments          ";
-    }
+        }
+        else if(ulifeinDays<pdays){
+            document.getElementById('Dout2').innerHTML = "#........Your given evaluated time period is greater than the useful life of "+assetName+" .....#";
+            document.getElementById('Dout3').innerHTML = "Residual Value : Rs."+residual;
+        }else{
+            //no adjustments
+            document.getElementById('Dout2').innerHTML = "         No adjustments          ";
+        }
+       
+    
+   
    
 }
 
